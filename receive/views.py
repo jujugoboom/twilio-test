b0c97316def2b427f3b03124ca49ff6b819ea546
Justin Covell <justincovell@Justins-MacBook>
2016-07-10 02:16:06 -0400

    First commit

--- receive/views.py
+++ receive/views.py
@@ -32,6 +32,20 @@ def personal(request):
         response = twilio.twiml.Response()
         response.message(numbers)
         return response
+    elif body == "last":
+        allnumbers = client.phone_numbers.list()
+        latest = client.messages.list(to=allnumbers[0].phone_number)[0]
+        for number in allnumbers:
+            messages = client.messages.list(to=number.phone_number)
+            for message in messages:
+                if message.date_sent > latest.date_sent:
+                    latest = message
+        response = twilio.twiml.Response()
+        message = "From: %s To: %s Message: %s" % (latest.from_,
+                                                   latest.to,
+                                                   latest.message)
+        response.message(message)
+        return response
 
 
 '''def forward(request):'''
