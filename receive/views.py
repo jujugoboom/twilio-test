ec4f00343a1ef8ade578baa9c8305e2c9bae9515
Justin Covell <justincovell@Justins-MacBook>
2016-07-10 00:30:35 -0400

    First commit

--- receive/views.py
+++ receive/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import twilio.twiml
 
 # Create your views here.
 
 
+@csrf_exempt
 def handle(request):
     response = twilio.twiml.Response()
     response.message("Thanks for the message")
