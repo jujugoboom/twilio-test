3bb09b50e62444ba081df7e198d5f623fd3767c9
Justin Covell <justincovell@Justins-MacBook>
2016-07-10 02:34:25 -0400

    First commit

--- receive/views.py
+++ receive/views.py
@@ -4,6 +4,7 @@ import twilio.twiml
 from twilio.rest import TwilioRestClient
 import random
 import os
+import phonenumbers
 
 # Create your views here.
 
@@ -17,6 +18,8 @@ def handle(request):
     from_number = request.POST.get("From")
     if from_number == PERSONAL_NUMBER:
         return HttpResponse(personal(request))
+    else:
+        return HttpResponse(forward(request))
     response = twilio.twiml.Response()
     response.message(str(random.randint(1, 2)))
     return HttpResponse(response)
@@ -33,19 +36,50 @@ def personal(request):
         response.message(numbers)
         return response
     elif body == "last":
-        allnumbers = client.phone_numbers.list()
-        latest = client.messages.list(to=allnumbers[0].phone_number)[0]
-        for number in allnumbers:
-            messages = client.messages.list(to=number.phone_number)
-            for message in messages:
-                if message.date_sent > latest.date_sent:
-                    latest = message
+        messages = client.messages.list(to=request.POST.get("To"))
+        latest = messages[0]
+        for message in messages:
+            if message.date_sent > latest.date_sent:
+                latest = message
         response = twilio.twiml.Response()
         message = "From: %s To: %s Message: %s" % (latest.from_,
                                                    latest.to,
                                                    latest.message)
         response.message(message)
         return response
+    else:
+        total = body.split()
+        to = convert_to_e164(total[0])
+        from_number = request.POST.get("To")
+        client.messages.create(from_=from_number, to=to, body=total[1:])
+        response = twilio.twiml.Response()
+        message = "Sent message \"%s\" to %s from %s" % (total[1:],
+                                                         to,
+                                                         from_number)
+        response.message(message)
+        return response
 
 
-'''def forward(request):'''
+def forward(request):
+    body = request.POST.get("Body")
+    message_from = request.POST.get("From")
+    message_to = request.POST.get("To")
+    message = "Got message \"%s\" from %s" (body, message_from)
+    client.messages.create(from_=message_to, to=PERSONAL_NUMBER, body=message)
+    return twilio.twiml.Response()
+
+
+def convert_to_e164(raw_phone):
+    if not raw_phone:
+        return
+
+    if raw_phone[0] == '+':
+        # Phone number may already be in E.164 format.
+        parse_type = None
+    else:
+        # If no country code information present, assume it's a US number
+        parse_type = "US"
+
+    phone_representation = phonenumbers.parse(raw_phone, parse_type)
+    return phonenumbers.format_number(phone_representation,
+                                      phonenumbers.PhoneNumberFormat.E164)
