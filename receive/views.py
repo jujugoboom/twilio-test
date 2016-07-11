e8682db1a8f4527fd488e1eac88a137d3d540860
Justin Covell <justincovell@Justins-MacBook>
2016-07-11 10:40:31 -0400

    Set possible responses

--- receive/views.py
+++ receive/views.py
@@ -20,7 +20,7 @@ def handle(request):
         return HttpResponse(personal(request))
     else:
         return HttpResponse(forward(request))'''
-    possible = ['1'] * 50 + ['2'] * 50 + ["one"] * 5 + ["two"] * 5 + ['3'] * 5
+    possible = ['1'] * 20 + ['2'] * 20 + ["one"] * 5 + ["two"] * 5 + ['3'] * 5
     response = twilio.twiml.Response()
     response.message(str(random.choice(possible)))
     return HttpResponse(response)
